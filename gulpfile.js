d7fb399f8f5ad31a12915f78bc2ab0a262cef699
DESKTOP-CDS32PI\wadah <nguyen@wadakoubou.com>
2021-11-19 14:27:15 +0900

    11

--- gulpfile.js
+++ gulpfile.js
@@ -1,117 +1,170 @@
 // gulpプラグインの読み込み
-var gulp = require('gulp');
-// エラーによる停止を防止するプラグインの読み込み
-var plumber = require('gulp-plumber');
-// デスクトップ通知のプラグインの読み込み
-var notify = require('gulp-notify');
-// Sassをコンパイルするプラグインの読み込み
-var sass = require('gulp-sass');
-// Sassを開発ツールで参照出来るようにするプラグインの読み込み
-var sourcemaps = require('gulp-sourcemaps');
-// パグ
-var pug = require('gulp-pug');
-// ブラウザシンク
-var browsersync = require('browser-sync');
-//画像の圧縮
-var changed = require('gulp-changed');
-var imagemin = require('gulp-imagemin');
-var imageminJpg = require('imagemin-jpeg-recompress');
-var imageminPng = require('imagemin-pngquant');
-var imageminGif = require('imagemin-gifsicle');
-var svgmin = require('gulp-svgmin');
+const gulp = require('gulp'),
+  // エラーによる停止を防止するプラグインの読み込み
+  plumber = require('gulp-plumber'),
+  // デスクトップ通知のプラグインの読み込み
+  notify = require('gulp-notify'),
+  // Sassをコンパイルするプラグインの読み込み
+  sass = require('gulp-sass'),
+  // Sassを開発ツールで参照出来るようにするプラグインの読み込み
+  sourcemaps = require('gulp-sourcemaps'),
+  // mediaqueryをまとめる
+  cmq = require('gulp-combine-media-queries'),
+  // パグ
+  pug = require('gulp-pug'),
+  // ブラウザシンク
+  browserSync = require('browser-sync'),
+  //画像の圧縮
+  changed  = require('gulp-changed'),
+  imagemin = require('gulp-imagemin'),
+  imageminJpg = require('imagemin-jpeg-recompress'),
+  imageminPng = require('imagemin-pngquant'),
+  imageminGif = require('imagemin-gifsicle'),
+  svgmin = require('gulp-svgmin');
 
 
 
 // パスの設定
 var paths = {
-  src: 'src',
-  dest: './'
-}
+  rootDir : 'htdocs',
+  pugDir : 'src/pug',
+  cssDir : 'htdocs/common/css',
+  scssDir : 'src/css',
+  srcImgDir : 'src/img',
+  dstImgDir : 'htdocs/common/img',
+  styleguideDir : 'src/styleguide'
+};
 
-//Pug
-gulp.task('html', function () {
-  return gulp.src([
-    paths.src + '/pug/**/*.pug',
-    '!' + paths.src + '/pug/**/_*.pug'
-  ])
-    .pipe(plumber({
-      errorHandler: notify.onError("Error: <%= error.message %>")
-    }))
-    .pipe(pug({
-      basedir: paths.src + '/pug/',
-      pretty: true
-    }))
-    .pipe(gulp.dest(paths.dest))
-});
+// パグ
+function fncPug(){
+  console.log("\n  pugを実行\n");
+	return gulp.src([paths.pugDir + "/**/*.pug", "!" + paths.pugDir + "/**/_*.pug"], {base: paths.pugDir})
+	.pipe(plumber({
+		errorHandler: notify.onError({
+			message: "ふぁいる→ <%= error.message %>",
+			title: "ぱぐのえらー",
+			icon: "error.png"
+		})
+	}))
+	.pipe(pug({
+		basedir: paths.pugDir,
+		pretty: true
+	}))
+	.pipe(gulp.dest(paths.rootDir));
+};
 
-//Sass
-gulp.task('css', function () {
-  return gulp.src([
-    paths.src + '/css/**/*.scss',
-    '!' + paths.src + '/css/**/_*.scss'
-  ])
-    .pipe(plumber({
-      errorHandler: notify.onError("Error: <%= error.message %>")
-    }))
-    // ソースマップを初期化
-    .pipe(sourcemaps.init())
-    .pipe(sass({
-        outputStyle: 'expanded'
-      })
-      .on('error', sass.logError)
-    )
-    // ソースマップを保存
-    .pipe(sourcemaps.write('./srcmap/'))
-    .pipe(gulp.dest(paths.dest + 'common/css'))
-});
+// scssのコンパイル
+function fncScss(){
+  console.log("\n  sassを実行\n");
+	// .scssファイルを取得
+	gulp.src([paths.scssDir + '/**/*.scss'])
+	// エラーでも止まらないように
+	.pipe(plumber({
+		// エラーをデスクトップ通知
+		errorHandler: notify.onError({
+			message: "ふぁいる→ <%= error.message %>",
+			title: "えすしーえすえすのえらー",
+			icon: "error.png"
+		})
+	}))
+	// ソースマップを初期化
+  .pipe(sourcemaps.init())
+	// Sassのコンパイルを実行
+	.pipe(sass({outputStyle: 'expanded'})
+	// Sassのコンパイルエラーを表示
+	// (これがないと自動的に止まってしまう)
+	.on('error', sass.logError))
+	// ソースマップを保存
+	.pipe(sourcemaps.write('./srcmap/'))
+	// cssフォルダー以下に保存
+	.pipe(gulp.dest(paths.cssDir));
+};
 
-//Browser Sync
-gulp.task('browser-sync', function (done) {
-  browsersync({
-    server: { //ローカルサーバー起動
-      baseDir: paths.dest
-    }
-  });
-  done();
-});
+// メディアクエリをまとめる
+function fncCmq(){
+	gulp.src([paths.cssDir + '/**/*.css'])
+	.pipe(cmq({
+		log: false
+	}))
+	.pipe(gulp.dest(paths.cssDir));
+};
 
 // jpg,png,gif画像の圧縮タスク
-gulp.task('image', function () {
-  console.log("gazou")
-  var srcGlob = paths.src + '/**/*.+(jpg|jpeg|png|gif)';
-  var dstGlob = paths.dest + '/common' ;
-  gulp.src(srcGlob)
-    .pipe(changed(dstGlob))
+function fncImagemin(){
+  console.log("\n  画像を圧縮\n");
+  var srcGlob = paths.srcImgDir + '/**/*.+(jpg|jpeg|png|gif)';
+  var dstGlob = paths.dstImgDir;
+  return gulp.src( srcGlob )
+    .pipe(changed( dstGlob ))
     .pipe(imagemin([
-        imageminPng(),
-        imageminJpg(),
-        imageminGif({
+      imageminPng(),
+      imageminJpg(),
+      imageminGif({
         interlaced: false,
         optimizationLevel: 3,
-        colors: 180
+        colors:180
       })
     ]))
-    .pipe(gulp.dest(dstGlob));
+    .pipe(gulp.dest( dstGlob ));
+};
+// svg画像の圧縮タスク
+function fncSvgmin(){
+  console.log("\n  SVGを圧縮\n");
+  var srcGlob = paths.srcImgDir + '/**/*.+(svg)';
+  var dstGlob = paths.dstImgDir;
+  return gulp.src( srcGlob )
+    .pipe(changed( dstGlob ))
+    .pipe(svgmin())
+    .pipe(gulp.dest( dstGlob ));
+};
+
+// ブラウザシンク
+function fncBrowserSync(done){
+  console.log("\n  BS立ち上げ\n");
+	browserSync({
+		server:{
+			baseDir: paths.rootDir,
+			index: "index.html"
+		}
+	});
+  done();
+};
+
+// ★ ファイルを監視
+gulp.task("watch", function(){
+  // ブラウザリロード
+  function bsReload(){
+    browserSync.reload();
+  }
+
+  gulp.watch(paths.pugDir + '/**/*.pug').on('change', fncPug);
+  gulp.watch(paths.scssDir + '/**/*.scss').on('change', fncScss);
+//	gulp.watch(paths.cssDir + '/**/*.css').on('change', gulp.series(fncCmq, bsReload));
+  gulp.watch(paths.rootDir + '/**/*.html').on('change', bsReload);
+  gulp.watch(paths.cssDir + '/**/*.css').on('change', bsReload);
+  gulp.watch(paths.srcImgDir + '/**/*', gulp.series(fncSvgmin, fncImagemin));
+  console.log("\n  監視を実行\n");
 });
 
-//Watch
-gulp.task('watch', function () {
-  const reload = () => {
-    browsersync.reload(); //リロード
-  };
-  gulp.watch(paths.src + '/css/**/*').on('change', gulp.series('css', reload));
-  gulp.watch(paths.src + '/pug/**/*').on('change', gulp.series('html', reload));
-  gulp.watch(paths.src + '/images/**/*').on('change', gulp.series('image', reload));
-});
+// npx gulpで実行
+exports.default = gulp.series(
+  gulp.parallel(
+    fncBrowserSync,
+    'watch'
+  )
+);
+//gulp.task('default',
+//  gulp.series(
+//    gulp.parallel(
+//      'browser-sync',
+//      'watch'
+//    )
+//  )
+//);
 
-//Default
-gulp.task('default',
-  gulp.series(
-    //'clean',
-    gulp.parallel(
-      'html',
-      'css',
-      'image',
-      'watch',
-      'browser-sync'
-    )));
+
+
+
+
+
+
